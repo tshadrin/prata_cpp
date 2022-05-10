2f967c9cc570fa9e581096c7a6ca76e6170ed264
sham <reg@shadrin.me>
2022-05-10 23:29:49 +0300

    add listing 3.1

---
+++ chapter3/listing3.1.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Анатолий Шадрин on 10.05.22.
+//
+#include <iostream>
+#include <climits>
+
+int main()
+{
+    using namespace std;
+    int n_int = INT_MAX;
+    short n_short = SHRT_MAX;
+    long n_long = LONG_MAX;
+    long long n_llong = LLONG_MAX;
+
+    cout << "int is " << sizeof (int) << " bytes." << endl;
+    cout << "short is" << sizeof n_short <<   "bytes." << endl;
+    cout << "long is" << sizeof n_long << " bytes." << endl;
+    cout << "long long is" << sizeof n_llong << " bytes." << endl;
+    return 0;
+}
