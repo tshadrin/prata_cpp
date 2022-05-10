313d2eaa1f597a049e8b19cd789355d72541f425
sham <reg@shadrin.me>
2022-05-10 18:59:45 +0300

    add listing 2.5

---
+++ chapter2/listing2.5.cpp
@@ -0,0 +1,24 @@
+//
+// Created by Анатолий Шадрин on 06.05.22.
+//
+#include <iostream>
+void simon(int);
+
+int main()
+{
+    using namespace std;
+    simon(3);
+    cout << "Введите целое число: ";
+    int count;
+    cin >> count;
+    simon(count);
+    cout << "Готово!" << endl;
+
+    return 0;
+}
+
+void simon(int n)
+{
+    using namespace std;
+    cout << "Саймон говорит потрогай свои игрушки " << n << " раз." << endl;
+}
