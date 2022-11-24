f776067ba636ba50810464be356ea261c1f662ca
tshadrin <tshadrin@gmail.com>
2022-11-24 17:35:26 +0300

    add exercises 5.10

---
+++ chapter5/exercise5.10.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    using namespace std;
+
+
+    cout << "Enter number of strings for display : " << endl;
+
+    int strings_count = 0;
+    cin >> strings_count;
+
+    for (int i = 1; i <= strings_count; i++) {
+        for (int j = strings_count - i; j > 0; j--) {
+            cout << '.';
+        }
+        for (int k = i; k > 0; k--) {
+            cout << '*';
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
