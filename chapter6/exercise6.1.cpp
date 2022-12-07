0a21c9f4c34503133afb8bfa7da431126a10f066
tshadrin <tshadrin@gmail.com>
2022-12-07 12:17:27 +0300

    add exercises 6.1-6.2

--- chapter6/exercise6.1.cpp
+++ chapter6/exercise6.1.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <cctype>
 
 int main()
 {
     using namespace std;
-
+    char ch;
+    while((ch = (char)cin.get()) != '@') {
+        if (islower(ch)) {
+            cout << (char)toupper(ch);
+        } else if(isupper(ch)) {
+            cout << (char)tolower(ch);
+        } else if (isdigit(ch)){
+            continue;
+        } else {
+            cout << ch;
+        }
+    }
+    cout << "\nBye, you entered @ symbol";
     return 0;
 }
--- chapter6/exercise6.2.cpp
+++ chapter6/exercise6.2.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include <array>
 
 int main()
 {
     using namespace std;
-    
+
+    const int ArSize = 10;
+    array <double, ArSize> values;
+    int count = 0;
+    double sum = 0.0;
+
+    for(; count < ArSize && cin >> values[count]; count++) {
+        sum += values[count];
+    }
+
+    if (count == 0) {
+        cout << "No values entered." << endl;
+        return 1;
+    }
+
+    double average = sum / count;
+    cout << "Average value is " << average << endl;
+    for (double value: values) {
+        if (value > average) {
+            cout << "This value > average " << value << endl;
+        }
+    }
     return 0;
 }
